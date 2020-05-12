120f8d8039d1024fcf38b07674869c845bbec3f3
Adam Cohen <mybirdcansing@gmail.com>
2020-05-12 15:10:40 -0700

    add copyright info

---
+++ web/src/components/layout/PageBottom.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="copyright">Copyright © 2019 - 2020 The Discoverables</div>
+</template>
+<script>
+    export default {
+        name: "PageBottom",
+    }
+</script>
+<style >
+    .copyright {
+        text-align: center;
+        height: 20px;
+        position: relative;
+    }
+</style>

7211d9354594cb8e7d8781799fe8a0fe9ac7ea32
Adam Cohen <mybirdcansing@gmail.com>
2020-06-10 18:31:18 -0700

    Added Configuration file for php lib

---
+++ web/lib/objects/Configuration.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+class Configuration
+{
+    private $_settings;
+    function __construct($configPath = __dir__ . '/../../../config/config.json')
+    {
+        $json = file_get_contents($configPath);
+        $json = preg_replace('/\s+/S', "", $json);
+        $this->_settings = json_decode($json);
+    }
+
+    function getSettings()
+    {
+        return $this->_settings;
+    }
+}
